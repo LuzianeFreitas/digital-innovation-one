ad2a68965132ecf04ea8ba5aa5df8fe1334dce43
LuzianeFreitas <freitas.lu@outlook.com>
2020-09-14 18:14:05 -0300

    Added integration telegram,youtube and dialog

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
+const dialogflow = require('./dialogflow');
+const youtube = require('./youtube');
 
 const token = '1342262228:AAFnnudjYfvNvaPxKg2gNpjCLUwEkVxJ9e0';
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', function (msg) {
+bot.on('message', async function (msg) {
     const chatId = msg.chat.id;
     console.log(msg.text);
-    bot.sendMessage(chatId, 'Obrigado por sua mensagem');
+
+    const dfResponse = await dialogflow.sendMessage(chatId.toString(), msg.text);
+
+    let responseText = dfResponse.text;
+
+    if(dfResponse.intent === 'Treino específico') {
+        responseText = await youtube.searchVideoURL(responseText, dfResponse.fields.corpo.stringValue);
+    }
+
+
+    bot.sendMessage(chatId, responseText);
 });
